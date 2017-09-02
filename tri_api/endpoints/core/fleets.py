6776a3bc70dc63eaeb59d37960bf2a05188faaa3
Eric Gisse <jowr.pi@gmail.com>
2017-09-02 03:26:58 +0000

    fleet comp voting

--- tri_api/endpoints/core/fleets.py
+++ tri_api/endpoints/core/fleets.py
@@ -1,192 +1,166 @@
-from flask import request
+from flask import request, Response, json
 from tri_api import app
 
-@app.route('/core/fleets/<int:char_id>/', methods=['GET'])
-def core_fleets(char_id):
-    from flask import Response, request
-    from json import dumps
+import common.logger as _logger
+import redis
 
-    import common.logger as _logger
-    import common.database as _database
-    import common.ldaphelpers as _ldaphelpers
-    import MySQLdb as mysql
-    import json
+# what the vote can be
 
+vote_classes = [ 'supers', 'titans', 'dreads', 'fax', 'carriers' ]
+
+@app.route('/core/fleets/active/<int:fleet_id>/vote', methods=['GET'])
+def fleet_vote(fleet_id):
+
+    # get the fleet composition voting results
+
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    ipaddress = request.headers['X-Real-Ip']
+    log_charid = request.args.get('log_charid')
+    _logger.securitylog(__name__, 'retrieved fleet vote', detail='fleet {0}'.format(fleet_id), ipaddress=ipaddress, charid=log_charid)
+
+    # redis does not actually connect above, i have to specifically test
+
+    error = False
     try:
-        sql_conn = mysql.connect(
-            database=_database.DB_DATABASE,
-            user=_database.DB_USERNAME,
-            password=_database.DB_PASSWORD,
-            host=_database.DB_HOST)
-    except mysql.Error as err:
-        _logger.log('[' + __name__ + '] mysql error: ' + str(err), _logger.LogLevel.ERROR)
-        js = json.dumps({'error': str(err)})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
+        r.client_list()
+    except redis.exceptions.ConnectionError as err:
+        msg = 'Redis connection error: {0}'.format(err)
+        error = True
+    except redis.exceptions.ConnectionRefusedError as err:
+        msg = 'Redis server offline'
+        error = True
+    except Exception as err:
+        msg = 'Redis generic error: {0}'.format(err)
+        error = True
 
-    cursor = sql_conn.cursor()
-
-    query = 'SELECT idCoreOpsBoard,Time,FC,Type,Doctrine,Hype,PostedBy,Scope,authgroup FROM OpsBoard WHERE Time > NOW() ORDER BY Time ASC'
-    try:
-        rowcount = cursor.execute(query)
-        rows = cursor.fetchall()
-    except mysql.Error as err:
-        _logger.log('[' + __name__ + '] mysql error: ' + str(err), _logger.LogLevel.ERROR)
-        js = json.dumps({'error': str(err)})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
-    finally:
-        cursor.close()
-
-    dn = 'ou=People,dc=triumvirate,dc=rocks'
-    filterstr = 'uid={}'.format(char_id)
-    attributes = ['corporation', 'authGroup', 'characterName', 'corporationName']
-    code, result = _ldaphelpers.ldap_search(__name__, dn, filterstr, attributes)
-
-    if code == False:
-        msg = 'unable to connect to ldap'
+    if error:
         _logger.log('[' + __name__ + '] {0}'.format(msg), _logger.LogLevel.ERROR)
         js = json.dumps({'error': msg})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
+        return Response(js, status=500, mimetype='application/json')
 
-    if result is not None:
+    # build the data structure back
 
-        (dn, info), = result.items()
+    result = dict()
 
-        name = info['characterName']
-        groups = info['authGroup']
-        corp = int(info['corporation'])
-        corpName = info['corporationName']
+    for ship_class in vote_classes:
 
-        fleets = []
+        # fetch the redis amount
+        try:
+            amount = r.get('{0}:vote:{1}'.format(fleet_id, ship_class))
+        except Exception as e:
+            msg = 'error fetching vote amounts from redis'
+            js = json.dumps({'error': msg})
+            return Response(js, status=500, mimetype='application/json')
 
-        for row in rows:
+        if not amount:
+            amount = 0
 
-            fleet_type = row[3]
+        result[ship_class] = int(amount)
 
-            if fleet_type == "THIRD PARTY FIGHT":
-                fleet_type = "FLEET"
+    js = json.dumps(result)
+    return Response(js, status=200, mimetype='application/json')
 
-            fleet = {
-                'id': row[0],
-                'time': row[1].isoformat(),
-                'fc': row[2],
-                'type': fleet_type,
-                'doctrine': row[4],
-                'text': row[5],
-                'by': row[6],
-                'modifiable': False
-            }
+@app.route('/core/fleets/active/<int:fleet_id>/vote/<int:char_id>/', methods=['GET', 'POST'])
+def fleet_char_vote(fleet_id, char_id):
 
-            if 'skyteam' in groups:
-                fleet['modifiable'] = True
-            elif name == row[6]:
-                fleet['modifiable'] = True
+    # get the fleet composition voting results from a specific character
 
-            scope = int(row[7])
+    r = redis.StrictRedis(host='localhost', port=6379, db=0)
+    ipaddress = request.headers['X-Real-Ip']
 
-            if scope == 1 and 'vanguard' in groups:
-                fleet['group'] = 'vanguard'
-                fleets.append(fleet)
-            elif scope == 2 and 'triumvirate' in groups:
-                fleet['group'] = 'triumvirate'
-                fleets.append(fleet)
-            elif scope == corp:
-                fleet['group'] = corpName
-                fleets.append(fleet)
-        return Response(json.dumps(fleets), status=200, mimetype='application/json')
-    else:
-        return Response({'error': 'not found'}, status=404, mimetype='application/json')
+    # redis does not actually connect above, i have to specifically test
 
-@app.route('/core/fleetspast/<int:char_id>/', methods=['GET'])
-def core_fleets_past(char_id):
-    from flask import Response, request
-    from json import dumps
-
-    import common.logger as _logger
-    import common.database as _database
-    import common.ldaphelpers as _ldaphelpers
-    import MySQLdb as mysql
-    import json
-
+    error = False
     try:
-        sql_conn = mysql.connect(
-            database=_database.DB_DATABASE,
-            user=_database.DB_USERNAME,
-            password=_database.DB_PASSWORD,
-            host=_database.DB_HOST)
-    except mysql.Error as err:
-        _logger.log('[' + __name__ + '] mysql error: ' + str(err), _logger.LogLevel.ERROR)
-        js = json.dumps({'error': str(err)})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
+        r.client_list()
+    except redis.exceptions.ConnectionError as err:
+        msg = 'Redis connection error: {0}'.format(err)
+        error = True
+    except redis.exceptions.ConnectionRefusedError as err:
+        msg = 'Redis server offline'
+        error = True
+    except Exception as err:
+        msg = 'Redis generic error: {0}'.format(err)
+        error = True
 
-    cursor = sql_conn.cursor()
-
-    query = 'SELECT idCoreOpsBoard,Time,FC,Type,Doctrine,Hype,PostedBy,Scope,authgroup FROM OpsBoard WHERE Time < NOW() ORDER BY Time DESC limit 10'
-    try:
-        rowcount = cursor.execute(query)
-        rows = cursor.fetchall()
-    except mysql.Error as err:
-        _logger.log('[' + __name__ + '] mysql error: ' + str(err), _logger.LogLevel.ERROR)
-        js = json.dumps({'error': str(err)})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
-    finally:
-        cursor.close()
-
-    dn = 'ou=People,dc=triumvirate,dc=rocks'
-    filterstr = 'uid={}'.format(char_id)
-    attributes = ['corporation', 'authGroup', 'characterName', 'corporationName']
-    code, result = _ldaphelpers.ldap_search(__name__, dn, filterstr, attributes)
-
-    if code == False:
-        msg = 'unable to connect to ldap'
+    if error:
         _logger.log('[' + __name__ + '] {0}'.format(msg), _logger.LogLevel.ERROR)
         js = json.dumps({'error': msg})
-        resp = Response(js, status=500, mimetype='application/json')
-        return resp
+        return Response(js, status=500, mimetype='application/json')
 
-    if result is not None:
+    if request.method == 'GET':
 
-        (dn, info), = result.items()
+        # build the data structure back
 
-        name = info['characterName']
-        groups = info['authGroup']
-        corp = int(info['corporation'])
-        corpName = info['corporationName']
+        result = dict()
 
-        fleets = []
+        for ship_class in vote_classes:
 
-        for row in rows:
+            # fetch the redis amount
+            try:
+                amount = r.get('{0}:vote:{1}:{2}'.format(fleet_id, char_id, ship_class))
+            except Exception as e:
+                msg = 'error fetching vote amounts from redis: {0}'.format(e)
+                _logger.log('[' + __name__ + '] {0}'.format(msg), _logger.LogLevel.ERROR)
+                js = json.dumps({'error': msg})
+                return Response(js, status=500, mimetype='application/json')
+            if not amount:
+                amount = 0
 
-            fleet_type = row[3]
+            result[ship_class] = int(amount)
 
-            if fleet_type == "THIRD PARTY FIGHT":
-                fleet_type = "FLEET"
+        js = json.dumps(result)
+        return Response(js, status=200, mimetype='application/json')
+    elif request.method == 'POST':
 
-            fleet = {
-                'id': row[0],
-                'time': row[1].isoformat(),
-                'fc': row[2],
-                'type': fleet_type,
-                'doctrine': row[4],
-                'text': row[5],
-                'by': row[6],
-                'modifiable': False
-            }
-            scope = int(row[7])
+        # *barf* i made a paplink
+        _logger.securitylog(__name__, 'voted fleet composition', detail='fleet {0}'.format(fleet_id), ipaddress=ipaddress, charid=char_id)
 
-            if scope == 1 and 'vanguard' in groups:
-                fleet['group'] = 'vanguard'
-                fleets.append(fleet)
-            elif scope == 2 and 'triumvirate' in groups:
-                fleet['group'] = 'triumvirate'
-                fleets.append(fleet)
-            elif scope == corp:
-                fleet['group'] = corpName
-                fleets.append(fleet)
-        return Response(json.dumps(fleets), status=200, mimetype='application/json')
-    else:
-        return Response({'error': 'not found'}, status=404, mimetype='application/json')
+        for ship_class in vote_classes:
+            new_amount = request.values.get(ship_class)
+            if not new_amount:
+                new_amount = 0
+
+            new_amount = int(new_amount)
+
+            # first set the character vote and overall vote to zero if no keys exist
+            r.setnx('{0}:vote:{1}:{2}'.format(fleet_id, char_id, ship_class), 0)
+            r.setnx('{0}:vote:{1}'.format(fleet_id, ship_class), 0)
+
+            if new_amount == 0:
+                continue
+
+            # get the current value just in case
+
+            current_amount = r.get('{0}:vote:{1}:{2}'.format(fleet_id, char_id, ship_class))
+            current_amount = current_amount.decode('utf-8')
+
+            if not current_amount:
+                current_amount = 0
+            current_amount = int(current_amount)
+
+            # next, we want to either increment or decrement
+
+            if new_amount < 0:
+                # decrement?
+                # cap the amount a user can peel off the overall vote to be no more
+                # than what they are already contributing
+
+                new_amount = min(current_amount, abs(new_amount))
+
+                if new_amount == 0:
+                    # do nothing
+                    continue
+
+                # peel off the amount from what the user has, and the overall vote
+                r.decr('{0}:vote:{1}'.format(fleet_id, ship_class), new_amount)
+                r.decr('{0}:vote:{1}:{2}'.format(fleet_id, char_id, ship_class), new_amount)
+
+            else:
+                # increment
+                r.incr('{0}:vote:{1}'.format(fleet_id, ship_class), new_amount)
+                r.incr('{0}:vote:{1}:{2}'.format(fleet_id, char_id, ship_class), new_amount)
+
+        js = json.dumps({})
+        return Response(js, status=200, mimetype='application/json')
+
