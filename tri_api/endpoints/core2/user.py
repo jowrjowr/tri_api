3f6be6112f708ba73d7e24e45fd731e28cfd5581
flyirian <flyirian@gmail.com>
2017-10-06 17:53:13 +0200

    core2 endpoints test

--- tri_api/endpoints/core2/user.py
+++ tri_api/endpoints/core2/user.py
@@ -4,5 +4,6 @@ from ..core2 import blueprint
 @blueprint.route('/<int:user_id>/', methods=['GET'])
 def user_endpoint(user_id):
     import flask
+    import json
 
-    return flask.Response({'user_id': user_id}, status=200, mimetype='application/json')
+    return flask.Response(json.dumps({'user_id': user_id}), status=200, mimetype='application/json')
