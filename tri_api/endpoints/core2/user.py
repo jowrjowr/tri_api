ed54856f23025746fd39009b40995a303e11ccaa
flyirian <flyirian@gmail.com>
2017-10-07 00:56:19 +0200

    core2 user endpoint

--- tri_api/endpoints/core2/user.py
+++ tri_api/endpoints/core2/user.py
@@ -1,6 +1,55 @@
 from ..core2 import blueprint
 
 
+def user_access_roles(groups, roles):
+    access = {
+        'routes': [],
+        'services': ['forum'],
+        'tools': []
+    }
+
+    if 'banned' in groups:
+        return dict()
+
+    # teamspeak
+    if 'triumvirate' in groups or 'vanguardBlue' in groups:
+        access['services'].append('teamspeak')
+
+    # discord & jabber
+    if 'triumvirate' in groups:
+        access['services'].append('discord')
+        access['services'].append('jabber')
+
+    # fleet stuff
+    if 'triumvirate' in groups or 'vanguardBlue' in groups:
+        access['routes'].append('opsboard')
+
+    # tri only
+    if 'triumvirate' in groups:
+        access['routes'].append('srp')
+
+    # broadcast
+    if 'Director' in roles or 'Personnel_Manager' in roles \
+        or 'skyteam' in groups or 'skirmishfc' in groups or 'administration' in groups:
+        access['tools'].append('broadcast')
+
+    # blacklist
+    if 'Director' in roles or 'Personnel_Manager' in roles:
+        access['tools'].append('blacklist')
+
+    #  corp tools
+    if 'command' in groups or 'administration' in roles:
+        access['tools'].append('corp_audit')
+        access['tools'].append('corp_structures')
+
+    #  alliance tools
+    if 'command' in groups or 'board' in groups:
+        access['tools'].append('alliance_audit')
+        access['tools'].append('alliance_structures')
+
+    return access
+
+
 @blueprint.route('/<int:user_id>/', methods=['GET'])
 def user_endpoint(user_id):
     import common.ldaphelpers as _ldaphelpers
@@ -27,6 +76,8 @@ def user_endpoint(user_id):
 
     (_, user), = result.items()
 
+    access_roles = user_access_roles(user['authGroup'], user['corporationRole'])
+
     return flask.Response(json.dumps(
         {
             'character_id': user['uid'],
@@ -35,6 +86,7 @@ def user_endpoint(user_id):
             'corporation_name': user['corporationName'],
             'alliance_id': user['alliance'],
             'alliance_name': user['allianceName'],
-            'groups': user['authGroup']
+            'groups': user['authGroup'],
+            'access': access_roles
         }
     ), status=200, mimetype='application/json')
