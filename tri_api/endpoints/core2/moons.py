fc042fa98a0e33a91cdc0023cdc77a6db6517cf7
flyirian <flyirian@gmail.com>
2017-10-07 21:27:09 +0200

    core2 moons endpoint

--- tri_api/endpoints/core2/moons.py
+++ tri_api/endpoints/core2/moons.py
@@ -13,7 +13,7 @@ def moons_post(user_id):
 
     logger = logging.getLogger(__name__)
 
-    lines = flask.request.get_data().splitlines()
+    lines = str(flask.request.get_data()).splitlines()
 
     regex_header = re.compile("(.*) (XC|XL|L?X{0,3})(IX|IV|V?I{0,3}) - Moon ([0-9]{1,3})")
     regex_lines = re.compile("\t(.*)\t([0-9]\.[0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)")
