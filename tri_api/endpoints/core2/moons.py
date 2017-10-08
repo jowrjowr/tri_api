e00f42c8b2bbacb6a21191a6a8c017b27b29cd11
flyirian <flyirian@gmail.com>
2017-10-08 02:42:58 +0200

    core2 moons endpoint

--- tri_api/endpoints/core2/moons.py
+++ tri_api/endpoints/core2/moons.py
@@ -15,7 +15,40 @@ def moons_post(user_id):
 
     lines = str(flask.request.get_data()).split('\\n')
 
-    regex_header = re.compile("(.*) (XC|XL|L?X{0,3})(IX|IV|V?I{0,3}) - Moon ([0-9]{1,3})")
-    regex_lines = re.compile("\t(.*)\t([0-9]\.[0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)")
+    regex_moon = re.compile("(.*) (XC|XL|L?X{0,3})(IX|IV|V?I{0,3}) - Moon ([0-9]{1,3})")
+    regex_mineral = re.compile("\t(.*)\t([0-9]\.[0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)\t([0-9]+)")
 
-    return flask.Response(regex_header.match(lines[1]).group(1))
+    moons = []
+
+    for i in range(0, len(lines)):
+        match = regex_moon.match(lines[i])
+
+        if match:
+            moon = {
+                'system_name': match.group(1),
+                'planet_name': match.group(2),
+                'moon_name': match.group(3),
+                'minerals': []
+            }
+
+            for j in range(0, 4):
+                match_mineral = regex_mineral.match(lines[i+j])
+
+                if not match_mineral:
+                    break
+
+                moon['system_id'] = match_mineral.group(4)
+                moon['planet_id'] = match_mineral.group(5)
+                moon['moon_id'] = match_mineral.group(6)
+
+                moon['minerals'].append({
+                    'product': match_mineral.group(1),
+                    'quantity': match_mineral.group(2),
+                    'ore_type': match_mineral.group(3),
+                })
+
+                i += 1
+
+            moons.append(moon)
+
+    return flask.Response(json.dumps(moons), status=200, mimetype='application/json')
