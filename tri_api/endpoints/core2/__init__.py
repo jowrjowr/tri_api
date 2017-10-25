5e9f671ca06ae183769207d5813b717428168895
flyirian <flyirian@gmail.com>
2017-10-25 03:14:48 +0200

    core2 moons missing endpoint

--- tri_api/endpoints/core2/__init__.py
+++ tri_api/endpoints/core2/__init__.py
@@ -1,3 +1,7 @@
 import flask as _flask
 
 blueprint = _flask.Blueprint("core2", __name__, url_prefix="/core2")
+
+from .moons import moons_get, moons_get_systems, moons_get_consts, moons_get_regions, moons_post, moons_get_conflicts
+from .moons import moons_get_missing, moons_post_conflicts_resolve, moons_get_scanners
+from .user import user_get
