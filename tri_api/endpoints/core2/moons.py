e6863b015cb0102627e3582c8dd125f6e8c98ebb
Andreas Weiss <flyirian@gmail.com>
2017-11-05 16:29:30 +0100

    moon endpoints

--- tri_api/endpoints/core2/moons.py
+++ tri_api/endpoints/core2/moons.py
@@ -454,7 +454,7 @@ def moons_get_structures(user_id):
 
     # get all characters that access & necessary scopes to structures
     dn = 'ou=People,dc=triumvirate,dc=rocks'
-    filterstr = '&((esiScope=\'esi-universe.read_structures.v1\')(corporationRole=Director))'
+    filterstr = '(&(esiScope=\'esi-universe.read_structures.v1\')(corporationRole=Director))'
     attrlist = ['uid', 'corporation']
 
     code, result = _ldaphelpers.ldap_search(__name__, dn, filterstr, attrlist)
